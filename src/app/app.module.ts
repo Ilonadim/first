09ccf3003a1112bfb07cc20d1fdb5f9e4e9a0d96
dim <ilona.demchyshyna@gmail.com>
2023-04-05 13:56:44 +0300

    new research project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
+import { WarningAllertComponent } from './warning-allert/warning-allert.component';
+import { SuccessAllertComponent } from './success-allert/success-allert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAllertComponent,
+    SuccessAllertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warning-allert/warning-allert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-allert',
+  templateUrl: './warning-allert.component.html',
+  styleUrls: ['./warning-allert.component.css']
+})
+export class WarningAllertComponent {
+
+}
