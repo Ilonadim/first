7305ccf440b853591733ea4b1e23954fa46ae3e8
dim <ilona.demchyshyna@gmail.com>
2023-04-06 19:19:00 +0300

    directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,17 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   //template: `
-    //<app-server></app-server>
+  //<app-server></app-server>
   //<app-server></app-server>`,
+
   templateUrl: 'servers.component.html',
   styleUrls: ['./servers.component.css']
+
 })
 export class ServersComponent implements OnInit {
 allowNewServer = false;
 serverCreationStatus = 'No server was created';
 serverName='testServer';
 userName = '';
-userNameStatus = 'anonymus';
+userNameStatus = 'anonymous';
+serverCreated = false;
+servers = [ 'Testserver', 'Testserver 2'];
+showDetails = false;
+log = [];
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,6 +29,8 @@ userNameStatus = 'anonymus';
 ngOnInit() {
 }
 onCreateServer () {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
 this.serverCreationStatus = 'Server was created! Name is ' +this.serverName;
 }
   onUpdateServerName(event: Event) {
@@ -30,5 +39,9 @@ this.serverName = (<HTMLInputElement>event.target).value;
 onAddUserName() {
     this.userNameStatus = 'Name was added! Your name is ' +this.userName;
     this.userName = '';
+}
+onToggleDetails() {
+    this.showDetails = !this.showDetails;
+    this.log.push(this.log.length + 1);
 }
 }
