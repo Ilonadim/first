18c5ad19889f1742c13b84f3c89dcd4c9b19d01e
dim <ilona.demchyshyna@gmail.com>
2023-05-12 23:46:31 +0300

    delete vs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  servers = [];
+  onAddServer() {
+    this.servers.push('Another Server');
+  }
+  omRemoveServer(id:number) {
+    const position = id + 1;
+    this.servers.splice(position,1);
+  }
   title = 'first';
 }
